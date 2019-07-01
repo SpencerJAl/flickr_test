caa944c092dd1fc90dff19dd2030346d597e4b06
SpencerJAl <gpmas117@gmail.com>
2019-07-01 23:38:26 +0100

    Task completed

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
-(function(){
+/**
+ * Created by gpmas_000 on 01/07/2019.
+ */
+(function () {
     'use strict';
 
-    angular.module('flickrApp', ['ngMaterial'])
-    .controller('ListController', ['$scope', '$http', function($scope, $http){
+    angular.module('App', ['ngMaterial'])
+        .controller('ListController', ['$scope', '$http', function ($scope, $http, success, error) {
+            $scope.results = [];
 
-        $scope.result = [];
+            $scope.search = function () {
+                $http({
+                    method: 'GET',
+                    url: 'https://api.flickr.com/services/rest',
+                    params: {
+                        method: 'flickr.photos.search',
+                        api_key: '1ddd9e91e1cd2f43d852d60464f951e3',
+                        text: $scope.searchTerm,
+                        format: 'json',
+                        nojsoncallback: 1
+                    }
 
-        $scope.isSearching = false;
+                }).then(function(success){
+                    $scope.results = success.data;
+                }, function errorCallBack(error) {
 
-        $scope.search = function(){
-            $scope.isSearching = true;
-            $http({
-                mehtod: 'GET',
-                url: 'https://api.flickr.com/services/rest',
-                params:{
-                    method:'flickr.photos.search',
-                    api_key: '1ddd9e91e1cd2f43d852d60464f951e3',
-                    text: $scope.searchTerm,
-                    format: 'json',
-                    nojsoncallback: 1
-                }
-            }).success(function(data){
-
-                $scope.result = data;
-                $scope.isSearching = false;
-
-            }).error(function(error){
-              console.error(error);
-              $scope.isSearching = false;
-   
-            });
-        }
-
-    }]);
-})
+                    console.error(error);
+                });
+            };
+        }]);
+})();
